f2f0e58c84111339bf9aa90a8add21be6ffd3a80
artembondar <artem.bondar@aa.com>
2019-09-22 18:02:52 -0500

    locators

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,9 +1,42 @@
-
+/// <reference types="cypress" />
 
 describe('Our first suite', () => {
 
     it('first test', () => {
 
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //by Tag Name
+        cy.get('input')
+
+        //by ID
+        cy.get('#inputEmail1')
+
+        //by Class name
+        cy.get('.input-full-width')
+
+        //by Attribute name
+        cy.get('[placeholder]')
+
+        //by Attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        //by Class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        //by Tag name and Attribute with value
+        cy.get('input[placeholder="Email"]')
+
+        //by two different attributes
+        cy.get('[placeholder="Email"][type="email"]')
+
+        //by tag name, Attribute with value, ID and Class name
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        //The most recommended way by Cypress
+        cy.get('[data-cy="imputEmail1"]')
     })
 
 })
