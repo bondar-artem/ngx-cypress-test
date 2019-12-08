c60f6ea4de150244b74f3a4dd24a484ac39166ba
artembondar <artem.bondar@aa.com>
2019-12-08 11:23:25 -0600

    find elements lesson

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -39,4 +39,28 @@ describe('Our first suite', () => {
         cy.get('[data-cy="imputEmail1"]')
     })
 
+    it.only('second test', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[data-cy="signInButton"]')
+
+        cy.contains('Sign in')
+
+        cy.contains('[status="warning"]','Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+        cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+
+    })
+
 })
