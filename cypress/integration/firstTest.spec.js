117e46fade36a80ce8d9f48a4b04454e2069c203
artembondar <artem.bondar@aa.com>
2019-12-14 12:31:07 -0600

    wrap and then commands

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -39,7 +39,7 @@ describe('Our first suite', () => {
         cy.get('[data-cy="imputEmail1"]')
     })
 
-    it.only('second test', () => {
+    it('second test', () => {
 
         cy.visit('/')
         cy.contains('Forms').click()
@@ -60,6 +60,47 @@ describe('Our first suite', () => {
             .click()
 
         cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+
+    })
+
+    it.only('then and wrap methods', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+        //selenium
+        // const fistForm = cy.contains('nb-card', 'Using the Grid')
+        // const secondForm = cy.contains('nb-card', 'Basic form')
+
+        // fistForm.find('[for="inputEmail1"]').should('contain', 'Email')
+        // fistForm.find('[for="inputPassword2"]').should('contain', 'Password')
+        // secondForm.find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //cypress style
+
+        cy.contains('nb-card', 'Using the Grid').then( firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then( secondForm => {
+                const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).to.equal(passwordSecondText)
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+            })
+
+        })
+
+        
 
     })
 
