4182b6a01913763bfb5fad549c29f981de1bfe55
artembondar <artem.bondar@aa.com>
2019-12-24 12:09:29 -0600

    сheckboxes

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -63,7 +63,7 @@ describe('Our first suite', () => {
 
     })
 
-    it.only('then and wrap methods', () => {
+    it('then and wrap methods', () => {
 
         cy.visit('/')
         cy.contains('Forms').click()
@@ -97,11 +97,87 @@ describe('Our first suite', () => {
                 cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
 
             })
+        })
+    })
 
+    it('invoke command', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //2
+        cy.get('[for="exampleInputEmail1"]').then( label => {
+            expect(label.text()).to.equal('Email address')
         })
 
-        
+        //3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr', 'class')
+            //.should('contain', 'checked')
+            .then(classValue => {
+                expect(classValue).to.contain('checked')
+            })
 
     })
+
+    it('assert property', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+            cy.wrap(input).click()
+            cy.get('nb-calendar-day-picker').contains('17').click()
+            cy.wrap(input).invoke('prop', 'value').should('contain', 'Dec 17, 2019')
+        })
+    })
+
+    it('radio button', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+            cy.wrap(radioButtons)
+                .first()
+                .check({force: true})
+                .should('be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({force: true})
+                
+            cy.wrap(radioButtons)
+                .eq(0)
+                .should('not.be.checked')
+                
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled')    
+        })
+    })
+
+    it.only('check boxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        //cy.get('[type="checkbox"]').check({force:true})
+        cy.get('[type="checkbox"]').eq(0).click({force:true})
+        cy.get('[type="checkbox"]').eq(1).check({force:true})
+
+    })
+
+
 
 })
