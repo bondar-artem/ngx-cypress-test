7383979803d0c34bcb7191108f21d41c944820cc
mykytenkovladislav <mykytenkovladislav@gmail.com>
2021-05-21 18:57:52 +0300

    second test (Finding Web Elements)

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -34,4 +34,35 @@ describe('our first suite', () => {
     // The most recommended way by Cypress
     cy.get('[data-cy="imputEmail1"]')
   })
+  it.only('second test', () =>{
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    // by adding own identifier to Sign In button
+    cy.get('[data-cy="signInButton"]')
+
+    //by text on element
+    cy.contains('Sign in')
+
+    //by unique Attribute name
+    cy.contains('[status="warning"]','Sign in')
+
+    /* What if it doesn't have any unique id or smth?
+    1. We will search first unique element id in section with needed item
+    2. We will jump to parents element
+    3. We will find child element with HTML tag button
+     */
+    cy.get('#inputEmail3')
+      .parents('form')
+      .find('button')
+      .should("contain", 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click()
+
+    //If we have unique only name of form
+    cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+
+  })
 })
