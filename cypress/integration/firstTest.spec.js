4e6432399010ca7193b0f37688020e9684d6232f
mykytenkovladislav <mykytenkovladislav@gmail.com>
2021-05-26 20:23:53 +0300

    fifth lesson(radio buttons)

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -126,15 +126,48 @@ describe('our first suite', () => {
         expect(classValue).to.contain('checked')
       })
   })
-  it.only('assert property', () => {
+  it('assert property', () => {
     cy.visit('/')
     cy.contains('Forms').click()
     cy.contains('Datepicker').click()
 
-    cy.contains('nb-card', 'Common Datepicker').find('input').then(input =>{
+    cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
       cy.wrap(input).click()
       cy.get('nb-calendar-day-picker').contains('24').click()
       cy.wrap(input).invoke('prop', 'value').should('contain', 'May 24, 2021')
     })
+  })
+  it.only('radio button', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+
+      cy.wrap(radioButtons)
+        .first()
+        .check({force: true})
+        .should('be.checked')
+
+      cy.wrap(radioButtons)
+        .eq(1)
+        .check({force: true})
+        .should('be.checked')
+
+      cy.wrap(radioButtons)
+        .first()
+        .should('not.be.checked')
+
+      cy.wrap(radioButtons)
+        .eq(2)
+        .should('be.disabled')
+    })
+
+  })
+  it.only('radio button', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
   })
 })
