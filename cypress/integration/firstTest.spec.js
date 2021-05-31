436cb56e76cdc7df16cb5fd336f0bbb0d771514c
mykytenkovladislav <mykytenkovladislav@gmail.com>
2021-05-31 12:44:49 +0300

    six lesson(lists of items)

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -173,8 +173,42 @@ describe('our first suite', () => {
     better to use it when you are working with checkboxes
      */
     // cy.get('[type="checkbox"]').check({force:true}) // it will check all checkboxes
-    cy.get('[type="checkbox"]').eq(0).click({force:true}).should('not.be.checked')
-    cy.get('[type="checkbox"]').eq(1).check({force:true}).should('be.checked')
+    cy.get('[type="checkbox"]').eq(0).click({force: true}).should('not.be.checked')
+    cy.get('[type="checkbox"]').eq(1).check({force: true}).should('be.checked')
 
+  })
+  it('lists and dropdowns', () => {
+
+    cy.visit('/')
+
+    // 1 way of working with single element
+    cy.get('nav nb-select').click()
+    cy.get('.options-list').contains('Dark').click()
+    cy.get('nav nb-select').should('contain', 'Dark')
+    cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+
+    //2
+    // what if we have a few options to check them?
+    cy.get('nav nb-select').then(dropdown => {
+      cy.wrap(dropdown).click()
+      cy.get('.options-list nb-option').each((listItem, index) => {
+        const itemText = listItem.text().trim()
+
+        const colors = {
+          "Light": "rgb(255, 255, 255)",
+          "Dark": "rgb(34, 43, 69)",
+          "Cosmic": "rgb(50, 50, 89)",
+          "Corporate": "rgb(255, 255, 255)"
+        }
+
+        cy.wrap(listItem).click()
+        cy.wrap(dropdown).should('contain', itemText)
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+        if (index < 3) {
+          cy.wrap(dropdown).click()
+        }
+      })
+    })
   })
 })
