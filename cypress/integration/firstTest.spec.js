1ac234fd3f7edc4b4033a5e170f2b55fbda83ed5
mykytenkovladislav <mykytenkovladislav@gmail.com>
2021-06-01 15:22:40 +0300

    lesson seven (web-tables)

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -218,11 +218,42 @@ describe('our first suite', () => {
 
     // 1
     // work with existing table rows
-    cy.get('tbody').contains('tr', 'Larry').then( tableRow =>{
+    cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
       cy.wrap(tableRow).find('.nb-edit').click()
       cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
       cy.wrap(tableRow).find('.nb-checkmark').click()
       cy.wrap(tableRow).find('td').eq(6).should("contain", '25')
+    })
+
+    // 2
+    // from example scenario: add new record
+    cy.get('thead').find('.nb-plus').click()
+    cy.get('thead').find('tr').eq(2).then(tableRow => {
+      cy.wrap(tableRow).find('[placeholder="First Name"]').type('Vlad')
+      cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Mykytenko')
+      cy.wrap(tableRow).find('.nb-checkmark').click()
+
+      cy.get("tbody tr").first().find('td').then(tableColumns => {
+        cy.wrap(tableColumns).eq(2).should('contain', 'Vlad')
+        cy.wrap(tableColumns).eq(3).should('contain', 'Mykytenko')
+      })
+    })
+
+    //3 filtering scenario
+
+    const age = [20, 30, 40, 200]
+
+    cy.wrap(age).each(age => {
+
+      cy.get('thead [placeholder="Age"]').clear().type(age)
+      cy.wait(500)
+      cy.get('tbody tr').each(tableRow => {
+        if (age === 200) {
+          cy.wrap(tableRow).should('contain', 'No data found')
+        } else {
+          cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+        }
+      })
     })
   });
 })
