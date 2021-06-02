14f0f0b3383340072c961c089f9442460868dbfd
mykytenkovladislav <mykytenkovladislav@gmail.com>
2021-06-02 09:01:52 +0300

    lesson eight (Datepickers)

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -211,7 +211,7 @@ describe('our first suite', () => {
       })
     })
   })
-  it.only('web-tables', () => {
+  it('web-tables', () => {
     cy.visit('/')
     cy.contains('Tables & Data').click()
     cy.contains('Smart Table').click()
@@ -256,4 +256,35 @@ describe('our first suite', () => {
       })
     })
   });
+  it.only('datepicker', () => {
+
+    function selectDayFromCurrent(day) {
+
+      let date = new Date()
+      date.setDate(date.getDate() + day)
+      let futureDay = date.getDate()
+      let futureMonth = date.toLocaleString('default', {month: 'short'})
+      let dateAssert = futureMonth+ ' ' + futureDay+ ", " + date.getFullYear()
+
+      cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+        if (!dateAttribute.includes(futureMonth)) {
+          cy.get('[data-name="chevron-right"]').click()
+          selectDayFromCurrent()
+        } else {
+          cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+        }
+      })
+      return dateAssert
+    }
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Datepicker').click()
+
+    cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+      cy.wrap(input).click()
+      let dateAssert = selectDayFromCurrent(15)
+      cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
+    })
+  })
 })
