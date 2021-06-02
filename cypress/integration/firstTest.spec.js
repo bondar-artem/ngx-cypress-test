0874aa2059f6b9f2dfe62a7db3e4d5dea26a5eaa
mykytenkovladislav <mykytenkovladislav@gmail.com>
2021-06-02 11:28:08 +0300

    small fix

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -134,7 +134,7 @@ describe('our first suite', () => {
     cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
       cy.wrap(input).click()
       cy.get('nb-calendar-day-picker').contains('24').click()
-      cy.wrap(input).invoke('prop', 'value').should('contain', 'May 24, 2021')
+      cy.wrap(input).invoke('prop', 'value').should('contain', 'Jun 24, 2021')
     })
   })
   it('radio button', () => {
