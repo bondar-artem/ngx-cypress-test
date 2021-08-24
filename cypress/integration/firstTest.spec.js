5e3c5794e84a6a5f93b3345aec2901838b58f665
Milos Lazic <laziclm89@gmail.com>
2021-08-25 00:39:07 +0200

    promena koda

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -15,26 +15,18 @@ describe("Our first suite from TL", () => {
     //ClassName
     cy.get(".input-full-width");
 
-    //AttributeName
     cy.get("[placeholder]");
 
-    //AttributeNameAndValue
-    cy.get('placeholder="Email"');
+    cy.get("[placeholder=Email]");
 
-    //ClassValue
-    cy.get('class="input-full-width size-medium shape-rectangle"');
+    cy.get('[class ="input-full-width size-medium shape-rectangle"]');
 
-    //Tag name and attribute with value
-    cy.get('input[placeholder="Email"]');
+    cy.get('input[placeholder = "Email"]');
 
-    //by two diferent attributes
     cy.get('[placeholder="Email"][type="email"]');
 
-    //by tag name, attribute with value, ID and Class name
+    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width');
 
-    cy.get('input[placeholder="Email][#inputEmail1][.input-full-width]');
-
-    //Najbolji nacin
-    cy.get('data-cy="imputEmail1"');
+    cy.get('[data-cy="imputEmail1"]');
   });
 });
