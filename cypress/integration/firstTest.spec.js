0c9afc38fef3e12826ecd5ea250a285f7bf4a965
Milos Lazic <laziclm89@gmail.com>
2021-08-28 15:22:04 +0200

    Check boxes test

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -100,24 +100,50 @@ describe("Our first suite from TL", () => {
     });
   });
 
-  //it.only("invoke command", () => {
-  //cy.visit("/");
-  //cy.contains("Forms").click();
-  // cy.contains("Form Layouts").click();
-
-  it.only(" assert property", () => {
+  it(" assert property", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Datepicker").click();
-
     cy.contains("nb-card", "Common Datepicker")
       .find("input")
       .then((input) => {
         cy.wrap(input).click();
+
         cy.get("nb-calendar-day-picker").contains("28").click();
         cy.wrap(input)
           .invoke("prop", "value")
           .should("contain", "Aug 28, 2021");
       });
+  });
+
+  it(" radio buttons", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid")
+      .find('[type = "radio"]')
+      .then((radioButtons) => {
+        cy.wrap(radioButtons)
+          .first()
+          .check({ force: true })
+          .should("be.checked");
+
+        cy.wrap(radioButtons).eq(1).check({ force: true });
+
+        cy.wrap(radioButtons).first().should("not.be.checked");
+
+        cy.wrap(radioButtons).eq(2).should("be.disabled");
+      });
+  });
+
+  it.only("check boxes", () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Toastr").click();
+
+    //cy.get('[type="checkbox"]').check({ force: true });
+    cy.get('[type="checkbox"]').eq(0).click({ force: true });
+    cy.get('[type="checkbox"]').eq(1).check({ force: true });
   });
 });
