e072a72a43d1a592ed0ca3a172381e35afd8bd14
smajumdar927 <58828730+smajumdar927@users.noreply.github.com>
2021-12-28 20:55:51 -0500

    added to develop branch

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,11 @@
 /// <reference types = "Cypress"/>
 
 describe("Test Suit", () => {
+    before(() => {
+        cy.viewport(1366,768)
+    })
     it("test spec 01", () => {
-        
+        cy.visit("http://localhost:4200/pages")
+        cy.contains("Forms").click()
     })
 })
