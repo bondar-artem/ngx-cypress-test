abc16c1e6cbeeaef4a597839a2b5eb1f6bb71650
Milos Lazic <laziclm89@gmail.com>
2022-01-08 00:11:09 +0100

    new test

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,27 +1,57 @@
 /// <reference types = "cypress" />
 
-describe("Our first suite", () => {
-  it("first test", () => {
+describe("Login page and login data filled", () => {
+
+  it("Form login page test1", () => {
+
     cy.visit("/")
+    cy.contains("Forms").click()
+    cy.contains('Form Layout').click()
+
+
+    //Add email and password info
+    cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+      .click()
+      .type('milos@gmail.com')
+
+    //input password field
+    cy.contains('nb-card', 'Horizontal form').find('#inputPassword3')
+      .click()
+      .type('smederevo026')
+
+    //check-box and sign in button activated
+    cy.get('#inputEmail3')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox').click()
+
+    cy.get('[status="warning"]').click()
+
+
+
+
 
     //by tag name
-    cy.get("input")
-
-    //by iD
-    cy.get("inputEmail1")
+    //y.get("input")
 
     //by class
-    cy.get(".input-full-width")
+    //cy.get(".input-full-width")
     //by attribute
-    cy.get("[placeholder]")
+    //cy.get("[placeholder]")
 
     //by atribute and value
-    cy.get('[placeholder="Email"]')
+    //cy.get('[placeholder="Email"]')
     //by class value
-    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+    //cy.get('[class="input-full-width size-medium shape-rectangle"]')
 
     //by tag name and atribute wiht value
 
-    cy.get('input[placeholder="Email"]')
+    //cy.get('input[placeholder="Email"]')
+
+    //
+    //cy.get('[data-cy="signInButton"]')
   })
+
 })
