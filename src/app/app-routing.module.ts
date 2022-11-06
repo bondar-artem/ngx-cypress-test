c30d95e1f3eeb92f97be6a0a44c146385c830091
asamamalik <asama.mashkraft@gmail.com>
2022-11-06 13:44:15 +0500

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import {
-  NbAuthComponent,
   NbLoginComponent,
   NbLogoutComponent,
   NbRegisterComponent,
