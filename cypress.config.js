80de77b9899f92815deabca053651289a463b104
Oleg_Sinkov <sinkovoleg088@gmail.com>
2023-05-17 16:58:02 +0200

    env var changed

--- cypress.config.js
+++ cypress.config.js
@@ -9,20 +9,20 @@ module.exports = defineConfig({
     user: "dummy",
     password: "dummy",
   },
-  reporter: 'cypress-multi-reporters',
+  reporter: "cypress-multi-reporters",
   reporterOptions: {
-    configFile: 'reporter-config.json',
+    configFile: "reporter-config.json",
   },
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   const user = process.env.USER_NAME;
-    //   const password = process.env.PASSWORD;
-    //   // if (!password || !user) {
-    //   //   //   throw new Error("You did not provide password or username");
-    //   //   // }
-    //   config.env = { user, password };
-    //   return config;
-    // },
+    setupNodeEvents(on, config) {
+      const user = process.env.USER_NAME;
+      const password = process.env.PASSWORD;
+      // if (!password || !user) {
+      //     throw new Error("You did not provide password or username");
+      //   }
+      config.env = { user, password };
+      return config;
+    },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     excludeSpecPattern: ["**/1-getting-started/*", "**/2-advanced-examples/*"],
   },
--- cypress/e2e/smoke/first-smoke.cy.js
+++ cypress/e2e/smoke/first-smoke.cy.js
@@ -1,15 +1,12 @@
 /// <reference types="cypress" />
-
-const cypress = require("cypress");
-
 describe("First smoke suite", () => {
   it("Smoke test", () => {
     cy.visit(Cypress.env("baseUrl") + "/pages");
     cy.contains("Layout").click();
     cy.contains("Stepper").click();
     cy.get("h3.ng-star-inserted").should("have.length", 2).and("be.visible");
-    cypress.log(Cypress.env("myPhrase"));
-    // cypress.log(Cypress.env('user'))
-    // cypress.log(Cypress.env('password'))
+    cy.log(Cypress.env("myPhrase"));
+    cy.log(Cypress.env("user"));
+    cy.log(Cypress.env("password"));
   });
 });
