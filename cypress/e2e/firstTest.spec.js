36dac3b61444bdc0555235a39db6163a93f78717
Artem Bondar <artem.bondar@gmail.com>
2023-09-17 12:31:54 -0500

    find web elements

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -36,6 +36,33 @@ describe('First test suite', () => {
         cy.get('[data-cy="imputEmail1"]')
     })
 
+    it.only('second test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //Theory
+        // get() - find elements on the page by locator globally
+        // find() - find child elements by locator
+        // contains() - find HTML text and by text and locator
+
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.contains('nb-card', 'Horizontal form').find('button')
+        cy.contains('nb-card', 'Horizontal form').contains('Sign in')
+        cy.contains('nb-card', 'Horizontal form').get('button')
+
+        //cypress chains and DOM
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+    }) 
+
 })
 
 
