4394dbb519b49acc5784f4af3988de8b40e000d7
Artem Bondar <artem.bondar@gmail.com>
2023-09-20 16:40:33 -0500

    lists and dropdowns

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -143,7 +143,7 @@ describe('First test suite', () => {
         cy.get('[type="checkbox"]').eq(1).check({force: true})
     })
 
-    it.only('Date picker', () => {
+    it('Date picker', () => {
 
         function selectDayFromCurrent(day){
             let date = new Date()
@@ -171,6 +171,28 @@ describe('First test suite', () => {
             const dateToAssert = selectDayFromCurrent(200)
             cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert)
             cy.wrap(input).should('have.value', dateToAssert)
+        })
+    })
+
+    it.only('Lists and dropdowns', () => {
+        cy.visit('/')
+
+        //1
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+
+        //2
+        cy.get('nav nb-select').then( dropDown => {
+            cy.wrap(dropDown).click()
+            cy.get('.options-list nb-option').each( (listItem, index) => {
+                const itemText = listItem.text().trim()
+                cy.wrap(listItem).click()
+                cy.wrap(dropDown).should('contain', itemText)
+                if( index < 3){
+                    cy.wrap(dropDown).click()
+                }
+            })
         })
     })
 
