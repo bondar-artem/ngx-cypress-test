be895faec1515192171dfd086c29f0d11d2e98c6
Artem Bondar <artem.bondar@gmail.com>
2023-09-22 11:53:45 -0500

    web tables 2

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -221,6 +221,24 @@ describe('First test suite', () => {
             cy.wrap(tableColumns).eq(3).should('contain', 'Smith')
         })
 
+       
+
+        //3 Get each row validation
+        const age = [20, 30, 40, 200]
+
+        cy.wrap(age).each( age => {
+            cy.get('thead [placeholder="Age"]').clear().type(age)
+            cy.wait(500)
+            cy.get('tbody tr').each( tableRow => {
+                if( age == 200){
+                    cy.wrap(tableRow).should('contain', 'No data found')
+                } else {
+                    cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+                }
+            })
+        })
+        
+
     })
 
 })
