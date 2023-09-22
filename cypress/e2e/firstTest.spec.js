779d24201dcab002f24a78622df8b06204e63730
Artem Bondar <artem.bondar@gmail.com>
2023-09-22 10:03:09 -0500

    web tables 1

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -174,7 +174,7 @@ describe('First test suite', () => {
         })
     })
 
-    it.only('Lists and dropdowns', () => {
+    it('Lists and dropdowns', () => {
         cy.visit('/')
 
         //1
@@ -194,6 +194,33 @@ describe('First test suite', () => {
                 }
             })
         })
+    })
+
+    it.only('Web tables', () => {
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1 Get the row by text
+        cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('35')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '35')
+        })
+
+        //2 Get row by index
+        cy.get('thead').find('.nb-plus').click()
+        cy.get('thead').find('tr').eq(2).then( tableRow => {
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type("John")
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type("Smith")
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+        })
+        cy.get('tbody tr').first().find('td').then( tableColumns => {
+            cy.wrap(tableColumns).eq(2).should('contain', 'John')
+            cy.wrap(tableColumns).eq(3).should('contain', 'Smith')
+        })
+
     })
 
 })
