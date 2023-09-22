8e1605d4e64cbebbb406a2c5c0e2f5aaf233b934
Artem Bondar <artem.bondar@gmail.com>
2023-09-22 17:13:21 -0500

    sync with udemy branch

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,33 @@
 const { defineConfig } = require('cypress')
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+//let percyHealthCheck = require('@percy/cypress/task')
 
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: false,
+  defaultCommandTimeout: 10000,
+  projectId: 'soeotu',
+  experimentalWebKitSupport: true,
+  env: {
+    'cypress-plugin-snapshots': {
+      imageConfig: {
+        threshold: 0.01,
+      },
+    },
+  },
   e2e: {
     baseUrl: 'http://localhost:4200',
-    excludeSpecPattern: ['**/1-getting-started', '**/2-advanced-examples'],
+    excludeSpecPattern: [
+      '**/1-getting-started', 
+      '**/2-advanced-examples',
+      '**/__snapshots__/*',
+      '**/__image_snapshots__/*',
+    ],
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      initPlugin(on, config);
+      //on("task", percyHealthCheck);
     },
   },
 });
