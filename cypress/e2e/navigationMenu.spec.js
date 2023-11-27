e6fa1f83d3643e9ccc47fdec6841cc4df8831da1
Oleksandr Ostapchuk <sanyokthebest2@gmail.com>
2023-11-26 19:21:39 -0500

    validate navigation menu

---
+++ cypress/e2e/navigationMenu.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+import NavigationPage from "../support/page_objects/navigation";
+import navigationMenu from "../support/static/navigation";
+const navigateTo = new NavigationPage()
+
+describe('Validate Navigation Menu', ()=>{
+  beforeEach(()=>{
+    cy.visit('/')
+  })
+
+  it('Navigate to pages',()=>{
+    navigateTo.formLayoutsPage()
+    navigateTo.datePickerPage()
+    navigateTo.toastrPage()
+    navigateTo.smartTablePage()
+  })
+
+  // Loop through all menu items
+  Object.values(navigationMenu).forEach(groupMenu=>{
+    Object.values(groupMenu.options).forEach(menuOption =>{
+      it(`Select ${groupMenu.name} -> ${menuOption}`,()=>{
+        navigateTo.selectGroupMenuItem(groupMenu.name)
+        cy.contains(menuOption).click()
+      })
+    })
+  })
+})
---
+++ cypress/support/static/navigation.js
@@ -0,0 +1,28 @@
+const navigationMenu = {
+  layout: {
+    name: 'Layout',
+    options: ['Stepper', 'Accordion']
+  },
+  forms: {
+    name: 'Forms',
+    options: ['Form Layouts', 'Datepicker']
+  },
+  modalsAndOverlays: {
+    name: 'Modal & Overlays',
+    options: ['Dialog', 'Window', 'Popover', 'Toastr', 'Tooltip']
+  },
+  extraComponents: {
+    name: 'Extra Components',
+    options: ['Calendar']
+  },
+  tableAndData: {
+    name: 'Tables & Data',
+    options: ['Smart Table', 'Tree Grid']
+  },
+  auth: {
+    name: 'Auth',
+    options: ['Login', 'Register', 'Request Password', 'Reset Password']
+  },
+}
+
+export default navigationMenu;
